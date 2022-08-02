9012517e9b79cfccd16d18fd4cbfcb7a1e7846e2
felipeSantosf <69824081+felipeSantosf@users.noreply.github.com>
2022-08-01 22:26:12 -0300

    Delete 01_inicio directory

--- 01_inicio/desafio_area.js
+++
@@ -1,7 +0,0 @@
-// PR * r
-
-let raio = 10;
-const PI = Math.PI;
-let circunferencia = (raio**2) * PI;
-
-console.log("O valor da circunferência é: " + circunferencia + "m2.")
--- 01_inicio/sentenca_2.js
+++
@@ -1,8 +0,0 @@
-//comentário 1
-/*
-* Comentário 2
-*/
-console.log("Passo #01");
-console.log("Passo #02");
-console.log("Passo #03");
-console.log("Passo #04");
